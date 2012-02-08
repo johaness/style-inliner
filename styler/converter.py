bedc882e846132ca7fa90c16e9ea8ce55f2503d3
Dana Woodman <dana@danawoodman.com>
2012-02-08 14:49:54 -0800

    Refactoring Conversion class to just inline CSS.

--- styler/converter.py
+++ styler/converter.py
@@ -1,175 +1,153 @@
-import os
-import sys
 import urllib
-import codecs
 import urlparse
-import csv
 import cssutils
-
-from lxml import etree
-from cssutils.script import csscombine
-from cssutils.script import CSSCapture
-from cssselect import CSSSelector, ExpressionError
+from lxml import etree, html
 
 from django.conf import settings
 
-class Conversion:
-    def __init__(self):
-        self.CSSErrors=[]
-        self.CSSUnsupportErrors=dict()
-        self.supportPercentage=100
-        self.convertedHTML=""
 
-    def perform(self,document,sourceHTML,sourceURL):
-        aggregateCSS="";
-            
-        # retrieve CSS rel links from html pasted and aggregate into one string
-        CSSRelSelector = CSSSelector("link[rel=stylesheet],link[rel=StyleSheet],link[rel=STYLESHEET]")
-        matching = CSSRelSelector.evaluate(document)
-        for element in matching:
-            try:
-                csspath=element.get("href")
-                if len(sourceURL):
-                    if element.get("href").lower().find("http://",0) < 0:
-                        parsedUrl=urlparse.urlparse(sourceURL);
-                        csspath=urlparse.urljoin(parsedUrl.scheme+"://"+parsedUrl.hostname, csspath)
-                f=urllib.urlopen(csspath)
-                aggregateCSS+=''.join(f.read())
-                element.getparent().remove(element)
-            except:
-                raise IOError('The stylesheet '+element.get("href")+' could not be found')
+class Converter:
+    """
+    Converts an HTML document to email compatible inline sytles.
+    
+    Removes any CSS in <link> or <style> tags an inlines them into the 
+    appropriate HTML tags.
+    
+    Usage::
+        html = '...' # Your HTML document here...
+        inlined_html = Converter(html).convert()
+    """
+    
+    def __init__(self, document, base_url=None):
+        # Attach the original document, the base CSS URL, the aggregated CSS and 
+        # the converted HTML so it can be accessed once the converter finishes 
+        # rendering the document.
+        self.document = html.fromstring(document)
+        self.base_url = base_url
+        self.aggregated_css = ''
+        self.converted_html = ''
+    
+    def convert(self):
+        """
+        Takes HTML of a document and inlines any CSS.
         
-        #include inline style elements
-        CSSStyleSelector = CSSSelector("style,Style")
-        matching = CSSStyleSelector.evaluate(document)
-        for element in matching:
-            aggregateCSS+=element.text
+        Optionally takes a `base_url` which is used to find the contents of 
+        any `<link>` elements.
+        """
+        self.aggregated_css = self._get_aggregated_css()
+        self.converted_html = self._inline_css()
+        
+        return self.converted_html
+    
+    def _get_aggregated_css(self):
+        """
+        Takes the document and parses out all the CSS from <link> and <style> 
+        elements.
+        """
+        css = self.aggregated_css
+        
+        # Retrieve CSS rel links from HTML pasted and aggregate into one string.
+        rel_links = 'link[rel=stylesheet],link[rel=StyleSheet],link[rel=STYLESHEET]'
+        for element in self.document.cssselect(rel_links):
+            try:
+                css_path = element.get('href')
+                
+                # If a base URL was passed, we attempt to find the resources 
+                # based off of that URL.
+                if base_url:
+                    if element.get('href').lower().find('http://', 0) < 0:
+                        parsed_url = urlparse.urlparse(base_url)
+                        css_path = urlparse.urljoin('%s://%s' % (
+                            parsed_url.scheme,
+                            parsed_url.hostname,
+                        ), css_path)
+            
+                # Grab the CSS from the URL.
+                f = urllib.urlopen(css_path)
+                css = css + ''.join(f.read())
+            
+                # Remove the <link> element from the HTML.
+                element.getparent().remove(element)
+            
+            except:
+                raise IOError('The stylesheet %s could not be found' % 
+                    element.get("href"))
+        
+        # Include inline style elements from <style> tags. Go through each 
+        # element, grab the CSS and then remove it after.
+        style_blocks = 'style,Style,STYLE'
+        for element in self.document.cssselect(style_blocks):
+            css = css + element.text
             element.getparent().remove(element)
         
-        #convert  document to a style dictionary compatible with etree
-        styledict = self.getView(document, aggregateCSS)
-        
-        #set inline style attribute if not one of the elements not worth styling
-        ignoreList=['html','head','title','meta','link','script']
-        for element, style in styledict.items():
-            if element.tag not in ignoreList:
-                v = style.getCssText(separator=u'')
-                element.set('style', v)
-        
-        #convert tree back to plain text html
-        self.convertedHTML = etree.tostring(document, method="xml", pretty_print=True,encoding='UTF-8')
-        self.convertedHTML= self.convertedHTML.replace('&#13;', '') #tedious raw conversion of line breaks.
-        
-        return self
-        
-    def styleattribute(self,element):
+        return css
+    
+    def _inline_css(self):
         """
-        returns css.CSSStyleDeclaration of inline styles, for html: @style
+        Takes the aggregated CSS and inlines it into the HTML.
         """
-        cssText = element.get('style')
-        if cssText:
-            return cssutils.css.CSSStyleDeclaration(cssText=cssText)
-        else:
-            return None
+        
+        # Stores all inlined elements.
+        elms = {}
+        
+        # Get all the CSS rules in a dictionary that we can operate on.
+        style_rules = cssutils.parseString(self.aggregated_css)
+        
+        for rule in style_rules:
             
-    def getView(self, document, css):
-        
-        view = {}
-        specificities = {}
-        supportratios={}
-        supportFailRate=0
-        supportTotalRate=0;
-        compliance=dict()
-        
-        #load CSV containing css property client support into dict
-        mycsv = csv.DictReader(open(os.path.join(settings.FILEROOT, "css_compliance.csv")), delimiter=',')
-        for row in mycsv:
-            #count clients so we can calculate an overall support percentage later
-            clientCount=len(row)
-            compliance[row['property'].strip()]=dict(row);
-        
-        #decrement client count to account for first col which is property name
-        clientCount-=1
-        
-        #sheet = csscombine(path="http://www.torchbox.com/css/front/import.css")
-        sheet = cssutils.parseString(css)
-        
-        rules = (rule for rule in sheet if rule.type == rule.STYLE_RULE)    
-        for rule in rules:
-            
-            for selector in rule.selectorList:
+            # Look through all elements that match this CSS selector.
+            if hasattr(rule, 'selectorText'):
+                
                 try:
-                    cssselector = CSSSelector(selector.selectorText)
-                    matching = cssselector.evaluate(document)
-                    
-                    for element in matching:
-                        # add styles for all matching DOM elements
-                        if element not in view:
-                            # add initial
-                            view[element] = cssutils.css.CSSStyleDeclaration()
-                            specificities[element] = {}
+                    for element in self.document.cssselect(rule.selectorText):
+                        
+                        # 
+                        if element not in elms:
+                            elms[element] = cssutils.css.CSSStyleDeclaration()
                             
-                            # add inline style if present
-                            inlinestyletext= element.get('style')
-                            if inlinestyletext:
-                                inlinestyle= cssutils.css.CSSStyleDeclaration(cssText=inlinestyletext)
+                            # Add existing inline style if present
+                            inline_styles = element.get('style')
+                            if inline_styles:
+                                inline_styles= cssutils.css.CSSStyleDeclaration(
+                                    cssText=inline_styles
+                                )
                             else:
-                                inlinestyle = None
-                            if inlinestyle:
-                                for p in inlinestyle:
-                                    # set inline style specificity
-                                    view[element].setProperty(p)
-                                    specificities[element][p.name] = (1,0,0,0)
-                                
+                                inline_styles = None
+                            if inline_styles:
+                                for p in inline_styles:
+                                    # Set inline style specificity
+                                    elms[element].setProperty(p)
+                        
+                        # Add the styles to the element.
                         for p in rule.style:
-                            #create supportratio dic item for this property
-                            if p.name not in supportratios:
-                                supportratios[p.name]={'usage':0,'failedClients':0}
-                            #increment usage
-                            supportratios[p.name]['usage']+=1
-                            
-                            try:
-                                if not p.name in self.CSSUnsupportErrors:
-                                    for client, support in compliance[p.name].items():
-                                        if support == "N" or support=="P":
-                                            #increment client failure count for this property
-                                            supportratios[p.name]['failedClients']+=1
-                                            if not p.name in self.CSSUnsupportErrors:
-                                                if support == "P":
-                                                    self.CSSUnsupportErrors[p.name]=[client + ' (partial support)']
-                                                else:
-                                                    self.CSSUnsupportErrors[p.name]=[client]
-                                            else:
-                                                if support == "P":
-                                                    self.CSSUnsupportErrors[p.name].append(client + ' (partial support)')
-                                                else:
-                                                    self.CSSUnsupportErrors[p.name].append(client)
-                                            
-                            except KeyError:
-                                pass
-                                                    
-                            # update styles
-                            if p not in view[element]:
-                                view[element].setProperty(p.name, p.value, p.priority)
-                                specificities[element][p.name] = selector.specificity
+                            if p not in elms[element]:
+                                elms[element].setProperty(p.name, p.value, p.priority)
                             else:
-                                sameprio = (p.priority == view[element].getPropertyPriority(p.name))
-                                if not sameprio and bool(p.priority) or (sameprio and selector.specificity >= specificities[element][p.name]):
-                                    # later, more specific or higher prio 
-                                    view[element].setProperty(p.name, p.value, p.priority)
-                                    
-                except ExpressionError:
-                    if str(sys.exc_info()[1]) not in self.CSSErrors:
-                        self.CSSErrors.append(str(sys.exc_info()[1]))
+                                # sameprio = (p.priority == view[element].getPropertyPriority(p.name))
+                                # if not sameprio and bool(p.priority) or (sameprio and selector.specificity >= specificities[element][p.name]):
+                                #     # later, more specific or higher prio 
+                                elms[element].setProperty(p.name, p.value, p.priority)
+                except:
+                    # TODO: Need to catch errors like ExpressionError here...
                     pass
         
-        for props, propvals in supportratios.items():
-            supportFailRate+=(propvals['usage']) * int(propvals['failedClients'])
-            supportTotalRate+=int(propvals['usage']) * clientCount
+        # Set inline style attributes unless the element is not worth styling.
+        ignore_list = [
+            'html',
+            'head',
+            'title',
+            'meta',
+            'link',
+            'script'
+        ]
+        for element, style in elms.items():
+            if element.tag not in ignore_list:
+                css = style.getCssText(separator=u'')
+                element.set('style', css)
         
-        if(supportFailRate and supportTotalRate):
-            self.supportPercentage= 100- ((float(supportFailRate)/float(supportTotalRate)) * 100)
-        return view
-
-class MyURLopener(urllib.FancyURLopener):
-    http_error_default = urllib.URLopener.http_error_default
+        # Convert tree back to plain a HTML string.
+        html = etree.tostring(self.document, method="xml", 
+            pretty_print=True, encoding='UTF-8')
+        
+        return html
+    
