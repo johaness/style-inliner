ed0dcd273642e871913b076f69ca6acec62be71e
Johannes Steger <jss@coders.de>
2013-03-06 21:13:20 +0100

    add an entry point for command line usage; make pretty printing optional; dont work on ignored elements;

--- inliner.py
+++ inliner.py
@@ -1,5 +1,5 @@
-import urllib
-import urlparse
+import sys
+
 import cssutils
 from lxml import etree, html
 
@@ -7,25 +7,36 @@ from lxml import etree, html
 ignore_list = ['html', 'head', 'title', 'meta', 'link', 'script']
 
 
-def inline_styles(document, css):
+def inline_styles(document, css, pretty=False):
     document = html.fromstring(document)
     elms = {} # stores all inlined elements.
     for rule in cssutils.parseString(css):
-        if hasattr(rule, 'selectorText'):
-            for element in document.cssselect(rule.selectorText):
-                if element not in elms:
-                    elms[element] = cssutils.css.CSSStyleDeclaration()
-                    inline_styles = element.get('style')
-                    if inline_styles:
-                        for p in cssutils.css.CSSStyleDeclaration(cssText=inline_styles):
-                            elms[element].setProperty(p)
+        if not hasattr(rule, 'selectorText'):
+            continue
+        for element in document.cssselect(rule.selectorText):
+            if element.tag in ignore_list:
+                continue
 
-                for p in rule.style:
-                    elms[element].setProperty(p.name, p.value, p.priority)
+            if element not in elms:
+                elms[element] = cssutils.css.CSSStyleDeclaration()
+                inline_styles = element.get('style')
+                if inline_styles:
+                    for p in cssutils.css.CSSStyleDeclaration(cssText=inline_styles):
+                        elms[element].setProperty(p)
 
-    # Set inline style attributes unless the element is not worth styling.
+            for p in rule.style:
+                elms[element].setProperty(p.name, p.value, p.priority)
+
     for element, style in elms.iteritems():
-        if element.tag not in ignore_list:
-            element.set('style', style.getCssText(separator=u''))
+        element.set('style', style.getCssText(separator=u''))
 
-    return etree.tostring(document, method="xml", pretty_print=True, encoding='UTF-8')
+    return etree.tostring(document, method="xml", pretty_print=pretty, encoding='UTF-8')
+
+
+def main():
+    assert len(sys.argv) == 4, \
+            """Usage: inline-styler SOURCE_HTML SOURCE_CSS OUTPUT_HTML"""
+    src_html, src_css, out_html = sys.argv[1:4]
+    res = inline_styles(file(src_html).read(), file(src_css).read())
+    with file(out_html, 'w') as outf:
+        outf.write(res)
